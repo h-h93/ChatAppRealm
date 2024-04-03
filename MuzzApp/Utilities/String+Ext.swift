d12d2325e4fdc0dedb6aa0ea827aea2154fcbc4f
h-93 <hanif234@hotmail.com>
2024-04-03 20:49:24 +0100

    added message sending animation

---
+++ MuzzApp/Utilities/String+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  String+Ext.swift
+//  MuzzApp
+//
+//  Created by hanif hussain on 03/04/2024.
+//
+
+import Foundation
---
+++ MuzzApp/previous.swift
@@ -0,0 +1,8 @@
+//
+//  previous.swift
+//  MuzzApp
+//
+//  Created by hanif hussain on 03/04/2024.
+//
+
+import Foundation
